878b6eefee38ab3a9da74f60899fcac3d80a8da3
jchandel-dev <jagritchandel2007@gmail.com>
2026-06-12 21:29:01 +0530

    my first commit

---
+++ jagrit.c
@@ -0,0 +1,18 @@
+// # include <stdio.h>
+// int main(){
+//     int n;
+//     printf("enter a number : ");
+//     scanf("%d",&n);
+//     if(n%3==0 || n%5==0){
+//         if(n%15!=0){
+//             printf("it is divisible by 5 and 3");
+//         }
+//         else{
+//             printf("it is  divisible by 15");
+//         }
+//     }
+//     else{
+//         printf("it is not divisible by 3 or 5");
+//     }
+//     return 0;
+// }
